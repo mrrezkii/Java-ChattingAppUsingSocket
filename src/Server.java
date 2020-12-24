ecbd7ea6252ca2ef1f63458cf08aec72e92c1456
Muhammad Rezki Ananda <muhammad.rezki.ananda@gmail.com>
2020-12-24 16:59:47 +0700

    Set framesize

---
+++ src/Server.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Server extends JFrame {
+
+    public Server(){
+        setSize(350, 600);
+        setLocation(200, 100);
+    }
+
+    public static void main(String[] args) {
+        new Server().setVisible(true);
+    }
+}
