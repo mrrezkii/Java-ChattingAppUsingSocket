b83187caaff383f11cb09af9e4ac3c02ed6ba1dd
Muhammad Rezki Ananda <muhammad.rezki.ananda@gmail.com>
2020-12-25 01:23:57 +0700

    Add image resource

--- src/Server.java
+++ src/Server.java
@@ -1,8 +1,18 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Server extends JFrame {
 
     public Server(){
+        ImageIcon srcBackArrow = new ImageIcon(ClassLoader.getSystemResource("Drawable/leftarrow.png"));
+        Image imgBackArrow = srcBackArrow.getImage().getScaledInstance(20, 20, Image.SCALE_DEFAULT);
+        ImageIcon mImgBackArrow = new ImageIcon(imgBackArrow);
+        JLabel lImageBackArrow = new JLabel(mImgBackArrow);
+        lImageBackArrow.setBounds(600, 0, 0, 0);
+        add(lImageBackArrow);
+
+
+        getContentPane().setBackground(Color.cyan);
         setSize(350, 600);
         setLocation(200, 100);
     }
